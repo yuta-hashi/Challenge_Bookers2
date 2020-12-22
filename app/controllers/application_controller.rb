8995005b4472235603971e7bc06fad44559dc3a6
橋本侑大 <hasi0620yuta02fact@gmail.com>
2020-12-22 23:26:59 +0900

    [Add]マイページを追加しました

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+
   protected
 
   def configure_permitted_parameters
@@ -9,4 +10,9 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
     devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
   end
+
+  def after_sign_in_path_for(resource)
+    user_path(resource)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   root to: 'homes#top'
+  resources :books
+  resources :users, only: [:show]
 end
