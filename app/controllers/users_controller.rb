4f47180dbb05010ca6c331b246dc3ecc1dff0c27
橋本侑大 <hasi0620yuta02fact@gmail.com>
2020-12-24 23:33:59 +0900

    [Add]部分テンプレートの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,15 @@
 class UsersController < ApplicationController
 
-
-  
   def index
     @users = User.all
   end
 
   def show
-    @user = current_user.id
+    @user = User.find(params[:id])
+  end
+
+  private
+  def book_params
+    params.require(:user).permit(:username, :introducution, :profile_image)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_23_134049) do
+ActiveRecord::Schema.define(version: 2020_12_24_142654) do
 
   create_table "books", force: :cascade do |t|
     t.text "book_title"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2020_12_23_134049) do
     t.datetime "updated_at", null: false
     t.string "username"
     t.string "profile_image_id"
+    t.text "introducution_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
