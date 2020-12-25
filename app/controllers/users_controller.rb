9592c4757a85ba9c61d11c2db2f69a4d5d476e51
橋本侑大 <hasi0620yuta02fact@gmail.com>
2020-12-25 23:51:27 +0900

    [Add]Bookページを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,14 +2,27 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+    @user = User.find(params[:id])
   end
 
   def show
     @user = User.find(params[:id])
+    @books = @user.books.page(params[:page]).reverse_order
+    @book = Book.new()
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
   end
 
   private
-  def book_params
+  def user_params
     params.require(:user).permit(:username, :introducution, :profile_image)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_many :books, dependent: :destroy
   attachment :profile_image
+
+  # インスタンスメソッド
+  def User.user_info
+    user = user.find()
+  end
 end
